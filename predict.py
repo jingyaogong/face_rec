8cce11e8fd2944c93cdb6188aca85fc7f8e5c801
JOYA GONG <joya.gong@calix.com>
2024-01-04 18:41:17 +0800

    update

---
+++ predict.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+from facenet import Facenet
+#
+# if __name__ == "__main__":
+#     model = Facenet()
+#     while True:
+#         image_1 = input('Input image_1 filename:')
+#         try:
+#             image_1 = Image.open(image_1)
+#         except:
+#             print('Image_1 Open Error! Try again!')
+#             continue
+#
+#         image_2 = input('Input image_2 filename:')
+#         try:
+#             image_2 = Image.open(image_2)
+#         except:
+#             print('Image_2 Open Error! Try again!')
+#             continue
+#
+#         probability = model.detect_image(image_1,image_2)
+#         print(probability)
+
+
+if __name__ == "__main__":
+    model = Facenet()
+    image_1 = 'img/1_001.jpg'
+    image_1 = Image.open(image_1)
+    probability = model.get_embedding(image_1)
+    print(probability)
